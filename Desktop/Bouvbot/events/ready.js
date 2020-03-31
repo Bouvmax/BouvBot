fd0cc2f025823088ec1e66901a1f28cf13103528
unknown <maxime.bouvet@live.fr>
2020-03-31 16:34:05 +0200

    Mise a jour du bot avec ajout de fonctionaliées

---
+++ Desktop/Bouvbot/events/ready.js
@@ -0,0 +1,1 @@
+module.exports = () => console.log("Je suis prêt !");
--- Desktop/Bouvbot/main.js
+++ Desktop/Bouvbot/main.js
@@ -1,13 +1,18 @@
 const Discord = require("discord.js");
+const { Collection, msg } = require("discord.js");
 const { CLE, PREFIX } = require("./config");
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("Je suis prêt !");
-});
+client.PREFIX = PREFIX;
 
-client.on("message", msg => {
-  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong !");
-});
+client.commands = new Collection();
+client.on("ready", () => require("./events/ready.js")(client));
+client.on("message", msg => require("./commands/message.js")(client, msg));
+client.on("romain", msg => require("./events/romain.js")(msg));
 
 client.login(CLE);
+
+client.on("debug", () => console.log);
+client.on("error", () => console.error);
+
+// Zone de test
